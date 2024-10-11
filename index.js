c34766c026c90ca95140cd605468bec2cc0a90dc
Yannick Camarata <yannickcamarata@hotmail.com>
2024-10-11 10:09:54 +0200

    ,

---
+++ index.js
@@ -0,0 +1,13 @@
+let monCanard = "donald";
+const nombreDePâtes ="2";
+const nombreDAile ="2";
+
+
+function canard(nombreDePâtes,nombreDAile){
+    let maCanette = "daisy";
+    let mesCanards = maCanette += monCanard;
+    /* return mesCanards; */
+    return nombreDePâtes += nombreDAile;
+}
+console.log((canard(nombreDePâtes,nombreDAile)));
+console.log((canard(2,1)));
