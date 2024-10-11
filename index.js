8611390a9789cda5ebf4dd11b2c35b2e5256f5d4
Yannick Camarata <yannickcamarata@hotmail.com>
2024-10-11 11:14:54 +0200

    ,

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const nombreDePâtes ="2";
 const nombreDAile ="2";
 
 
-function canard(nombreDePâtes,nombreDAile){
+/* function canard(nombreDePâtes,nombreDAile){
     let maCanette = "daisy";
     let mesCanards = maCanette += monCanard;
    /*   return mesCanards;  */
-     return nombreDePâtes += nombreDAile; 
-}
+   /*   return nombreDePâtes += nombreDAile;  */
+/* }
 console.log(canard(nombreDePâtes,nombreDAile));
 console.log((canard(2,1)));
 
@@ -16,4 +16,13 @@ function canard1(nombreDePâtes1,nombreDAile1){
     let monCanard1 = nombreDePâtes1 += nombreDAile1;
     return monCanard1;
 }
-console.log(canard1(2,2))
+console.log(canard1(2,2)) */ 
+
+let x ='1';
+
+function f (){
+    let x = 2;
+    console.log(x);
+}
+console.log(x);
+f()
