2a8f871f8ae2fb4797d637422cf89e5a2249052f
Yannick Camarata <yannickcamarata@hotmail.com>
2024-10-11 12:02:30 +0200

    function et boucle

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let monCanard = "donald";
-const nombreDePâtes ="2";
-const nombreDAile ="2";
+const nombreDePâtes = "2";
+const nombreDAile = "2";
 
 
 /* function canard(nombreDePâtes,nombreDAile){
     let maCanette = "daisy";
     let mesCanards = maCanette += monCanard;
    /*   return mesCanards;  */
-   /*   return nombreDePâtes += nombreDAile;  */
+/*   return nombreDePâtes += nombreDAile;  */
 /* }
 console.log(canard(nombreDePâtes,nombreDAile));
 console.log((canard(2,1)));
@@ -16,13 +16,38 @@ function canard1(nombreDePâtes1,nombreDAile1){
     let monCanard1 = nombreDePâtes1 += nombreDAile1;
     return monCanard1;
 }
-console.log(canard1(2,2)) */ 
+console.log(canard1(2,2)) */
 
-let x ='1';
+/* let x ='1';
 
 function f (){
     let x = 2;
     console.log(x);
 }
 console.log(x);
-f()
+f() */
+
+const lettre = "ABCDEFGHJKLMNPQRSTUVWXY";
+const nombre = "0123456789";
+let plaque = "";
+
+for (i = 0; i < 2; i++) {
+    plaque += lettre.charAt(Math.floor(Math.random() * (lettre.length)))
+
+}
+plaque += "-";
+/* console.log(plaque); */
+
+for (i = 0; i < 3; i++) {
+    plaque += nombre.charAt(Math.floor(Math.random() * (nombre.length)))
+
+}
+plaque += "-";
+/* console.log(plaque); */
+
+for (i = 0; i < 2; i++) {
+    plaque += lettre.charAt(Math.floor(Math.random() * (lettre.length)))
+
+}
+plaque;
+console.log(plaque);
